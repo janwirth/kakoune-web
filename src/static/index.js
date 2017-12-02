d13fb02b551233fc68e64f829b6c9b8fd7561309
Jan Wirth <jan.wirth@cinteo.com>
2017-12-02 09:37:13 +0100

    Basic kak interface

--- src/static/index.js
+++ src/static/index.js
@@ -1,8 +1,57 @@
 // pull in desired CSS/SASS files
 require( './styles/main.scss' );
-var $ = jQuery = require( '../../node_modules/jquery/dist/jquery.js' );           // <--- remove if jQuery not needed
-require( '../../node_modules/bootstrap-sass/assets/javascripts/bootstrap.js' );   // <--- remove if Bootstrap's JS not needed 
 
 // inject bundled Elm app into div#main
 var Elm = require( '../elm/Main' );
 Elm.Main.embed( document.getElementById( 'main' ) );
+
+
+var JsonRPC = require('simple-jsonrpc-js');
+
+//configure
+var jrpc = new JsonRPC();
+var socket = new WebSocket("ws://localhost:8090");
+
+//wait of call
+jrpc.on('ui.update', u => console.log(u))
+
+socket.onmessage = function(event) {
+    jrpc.messageHandler(event.data);
+};
+
+jrpc.toStream = function(_msg){
+    socket.send(_msg);
+};
+
+socket.onerror = function(error) {
+    console.error("Error: " + error.message);
+};
+
+socket.onclose = function(event) {
+    if (event.wasClean) {
+        console.info('Connection close was clean');
+    } else {
+        console.error('Connection suddenly close');
+    }
+    console.info('close code : ' + event.code + ' reason: ' + event.reason);
+};
+
+//usage
+//after connect
+socket.onopen = function(){
+
+    //calls
+    jrpc.call('add', [2, 3]).then(function (result) {
+        document.getElementsByClassName('paragraph')[0].innerHTML += 'add(2, 3) result: ' + result + '<br>';
+    });
+
+    jrpc.call('mul', {y: 3, x: 2}).then(function (result) {
+        document.getElementsByClassName('paragraph')[0].innerHTML += 'mul(2, 3) result: ' + result + '<br>';
+    });
+
+    jrpc.batch([
+        {call:{method: "add", params: [5,2]}},
+        {call:{method: "mul", params: [100, 200]}},
+        {call:{method: "create", params: {item: {foo: "bar"}, rewrite: true}}}
+    ]);
+};
--- start-server.js
+++ start-server.js
@@ -1,18 +1,97 @@
 const { spawn } = require('child_process');
-const ls = spawn('kak', ['-ui', 'json']);
+var WebSocketServer = new require('ws');
+var JsonRPC = require('simple-jsonrpc-js');
 
-const hello = () => ls.stdin.write('{ "jsonrpc": "2.0", "method": "keys", "params": ["iHello<esc>"] }')
-ls.stdout.on('data', (data) => {
-  console.log(`stdout: ${data}`);
-  setTimeout(hello, 1000)
-  
-});
 
-ls.stderr.on('data', (data) => {
-  console.log(`stderr: ${data}`);
-});
+const hello = () => ({ "jsonrpc": "2.0", "method": "keys", "params": ["iHello<esc>"] })
 
-ls.on('close', (code) => {
-  console.log(`child process exited with code ${code}`);
-});
+startServer()
 
+// startKak : ( KakUpdate -> a ) -> tell 
+function startKak (onData) {
+  const kak = spawn('kak', ['-ui', 'json'])
+  kak.stderr.on('data', data => onData(data.toString()) )
+  kak.stdout.on('data', data => onData(data.toString()) )
+
+  // tell : KakKeystrokes -> SideEffect@Kak
+  const tell = message => kak.stdin.write(JSON.stringify(message))
+  return tell
+}
+
+// startServer : () -> SideEffect@Server
+function startServer () {
+  const tellKak = startKak( data => console.log(data) )
+  setTimeout( () => tellKak(hello()), 1000 )
+}
+/*
+const kak = spawn('kak', ['-ui', 'json']);
+startServer()
+
+function startKak (callback) {
+
+  kak.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+    setTimeout(hello, 1000)
+    
+  });
+
+  kak.stderr.on('data', (data) => {
+    console.log(`stderr: ${data}`);
+  });
+
+  kak.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
+
+  // TELL
+  const tell = sth => kak.stdin.write(JSON.stringify(sth))
+  // LISTEN
+  kak.stdout.on('data', callback)
+
+  return tell
+}
+
+function startServer () {
+  function add(x, y){
+    return x + y;
+  }
+
+  //over ws
+
+  var webSocketServer = new WebSocketServer.Server({
+      host: '0.0.0.0',
+      port: 8090
+  }).on('connection', function(ws) {
+      var jrpc = new JsonRPC();
+      ws.jrpc = jrpc;
+
+      ws.jrpc.toStream = function(message){
+          ws.send(message);
+      };
+
+      ws.on('ui.keypress', function(message) {
+          jrpc.messageHandler(message);
+      });
+
+      jrpc.on('add', ['x', 'y'], add);
+
+      jrpc.on('mul', ['x', 'y'], function(x, y){
+          return x*y;
+      });
+
+      var item_id = 120;
+
+      jrpc.on('create', ['item', 'rewrite'], function(item, rewrite){
+          item_id ++;
+          item.id = item_id;
+          return item;
+      });
+
+      // KAKOUNE INTERFACE
+      jrpc.call('ui.update', ["starting..."]);
+      const callback = u => jrpc.call('kak.update', JSON.stringify(u))
+      const tellKak = startKak(callback)
+      setTimeout( x => tellKak(hello), 2000)
+  });
+}
+*/
